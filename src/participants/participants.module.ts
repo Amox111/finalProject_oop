beda1b68b39b1de900767bf24076cdf93342806f
Amox111 <ruxkwarmmana233@gmail.com>
2026-03-08 16:43:27 +0700

    update participants module

--- src/participants/participants.module.ts
+++ src/participants/participants.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ParticipantsController } from './participants.controller';
 import { ParticipantsService } from './participants.service';
+import { EventsModule } from '../events/events.module';
 
 @Module({
+  imports: [EventsModule],
   controllers: [ParticipantsController],
-  providers: [ParticipantsService]
+  providers: [ParticipantsService],
 })
 export class ParticipantsModule {}
